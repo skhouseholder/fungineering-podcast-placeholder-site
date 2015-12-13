94365ddde848fd6dc1f292e345c0e31c5e44dab1
Sarah Householder <skhouseholder@gmail.com>
2015-12-12 23:57:13 -0800

    Remove excess stuff, change colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
 (function($){
 	
-	$(window).on('load', function(){
-		$('.fade-in').css({ position: 'relative', opacity: 0, top: -14 });
-		setTimeout(function(){
-			$('#preload-content').fadeOut(400, function(){
-				$('#preload').fadeOut(800);
-				setTimeout(function(){
-					$('.fade-in').each(function(index) {
-						$(this).delay(400*index).animate({ top : 0, opacity: 1 }, 800);
-					});
-				}, 800);
-			});
-		}, 400);
-	});
+	// $(window).on('load', function(){
+	// 	$('.fade-in').css({ position: 'relative', opacity: 0, top: -14 });
+	// 	setTimeout(function(){
+	// 		$('#preload-content').fadeOut(400, function(){
+	// 			$('#preload').fadeOut(800);
+	// 			setTimeout(function(){
+	// 				$('.fade-in').each(function(index) {
+	// 					$(this).delay(400*index).animate({ top : 0, opacity: 1 }, 800);
+	// 				});
+	// 			}, 800);
+	// 		});
+	// 	}, 400);
+	// });
 
 	$(document).ready( function(){
 
 		// Create a countdown instance. Change the launchDay according to your needs.
 		// The month ranges from 0 to 11. I specify the month from 1 to 12 and manually subtract the 1.
 		// Thus the launchDay below denotes 23 March, 2015.
-		var launchDay = new Date(2015, 3-1, 23);
-		$('#timer').countdown({
-			until: launchDay
-		});
+		// var launchDay = new Date(2015, 3-1, 23);
+		// $('#timer').countdown({
+		// 	until: launchDay
+		// });
 
 		// Add background image
-		$.backstretch('images/bg1.jpg');
+		// $.backstretch('images/bg1.jpg');
 
 		// Invoke the Placeholder plugin
 		$('input, textarea').placeholder();
@@ -61,23 +61,23 @@
 		});
 
 		// Open modal window on click
-		$('#modal-open').on('click', function(e) {
-			var mainInner = $('#main .inner'),
-				modal = $('#modal');
+		// $('#modal-open').on('click', function(e) {
+		// 	var mainInner = $('#main .inner'),
+		// 		modal = $('#modal');
 
-			mainInner.animate({ opacity: 0 }, 400, function(){
-				$('html,body').scrollTop(0);
-				modal.addClass('modal-active').fadeIn(400);
-			});
-			e.preventDefault();
+		// 	mainInner.animate({ opacity: 0 }, 400, function(){
+		// 		$('html,body').scrollTop(0);
+		// 		modal.addClass('modal-active').fadeIn(400);
+		// 	});
+		// 	e.preventDefault();
 
-			$('#modal-close').on('click', function(e) {
-				modal.removeClass('modal-active').fadeOut(400, function(){
-					mainInner.animate({ opacity: 1 }, 400);
-				});
-				e.preventDefault();
-			});
-		});
+		// 	$('#modal-close').on('click', function(e) {
+		// 		modal.removeClass('modal-active').fadeOut(400, function(){
+		// 			mainInner.animate({ opacity: 1 }, 400);
+		// 		});
+		// 		e.preventDefault();
+		// 	});
+		// });
 
 	});
 	
